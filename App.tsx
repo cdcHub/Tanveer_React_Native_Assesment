813a1e4278b4554464685612b5b79b5e303dc35f
cdcHub <792tanveer@gmail.com>
2025-05-30 14:37:12 +0500

    android status bar color added

--- App.tsx
+++ App.tsx
@@ -3,10 +3,15 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import appStyles from '@theme/appStyle';
 import {ErrorModal, SafeContentArea} from '@components/index';
 import RootStack from '@navigation/RootStack';
+import {Platform, StatusBar} from 'react-native';
+import colors from '@theme/colors';
 
 const App: FC = () => {
   return (
     <SafeAreaProvider style={[appStyles.flex, appStyles.backgroundColor]}>
+      {Platform.OS === 'android' && (
+        <StatusBar backgroundColor={colors.primary} />
+      )}
       <SafeContentArea>
         <RootStack />
         <ErrorModal />
